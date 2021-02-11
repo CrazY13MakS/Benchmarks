ac988703e8b0ad61d94eb0376513fb67894c0713
Maks <HomerJSimpson91@yandex.ua>
2021-02-12 01:42:23 +0200

    GetHasCode vs MD5 vs Deterministic GetHashCode

---
+++ GetHashCodeVsMd5VsDeterministic/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace GetHashCodeVsMd5VsDeterministic
+{
+    internal static class Helper
+    {
+        public static int GetDeterministicHashCode(this string str)
+        {
+            unchecked
+            {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                return hash1 + (hash2 * 1566083941);
+            }
+        }
+
+        public static string GenerateString(int length)
+        {
+            Random random = new Random();
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            StringBuilder result = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(characters[random.Next(characters.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
